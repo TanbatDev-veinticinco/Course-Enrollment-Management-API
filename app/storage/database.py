45d5fbe7f50f9ce5e21011d7f296eed7f4e3e7b8
tanbatDev <omolayoalatishe@gmail.com>
2026-02-15 23:24:26 +0100

    created a test folder to run the test for each endpoint (TDD)

--- app/storage/database.py
+++ app/storage/database.py
@@ -9,6 +9,7 @@ user_id_counter = 1
 course_id_counter = 1
 enrollment_id_counter = 1
 
+
 def get_next_user_id() -> int:
     global user_id_counter
     current_id = user_id_counter
@@ -22,8 +23,22 @@ def get_next_course_id() -> int:
     course_id_counter += 1
     return current_id
 
+
 def get_next_enrollment_id() -> int:
     global enrollment_id_counter
     current_id = enrollment_id_counter
     enrollment_id_counter += 1
     return current_id
+
+
+def reset_database():
+    global users, courses, enrollments
+    global user_id_counter, course_id_counter, enrollment_id_counter
+
+    users.clear()
+    courses.clear()
+    enrollments.clear()
+
+    user_id_counter = 1
+    course_id_counter = 1
+    enrollment_id_counter = 1
